8d0aa84fc867f0d5e516c75d929db79dbfad2c11
Simon <simon.meggle@elabit.de>
2021-04-15 08:46:24 +0200

    Added Github URL

--- robotmk.py
+++ robotmk.py
@@ -5,6 +5,7 @@ import inspect
 class robotmk():
     """This is a small supplementary library for *Robotmk*, the Robot Framework integration project for *Checkmk*. 
     
+    - Github: https://github.com/simonmeggle/robotframework-robotmk
     - Author: Simon Meggle <simon.meggle@elabit.de>
     - Robotmk: https://www.robotmk.org
     - Checkmk: https://checkmk.com
